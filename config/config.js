5454e5c97d5ec1fb71310a591bd7d9c3851d95d8
Craig Derocher <craigderocher@Craig.local>
2019-03-12 20:23:28 -0500

    message

--- config/config.js
+++ config/config.js
@@ -7,9 +7,9 @@ const config = {
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/mernproject',
-  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
-  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
-  stripe_test_api_key: 'YOUR_stripe_test_api_key'
+  stripe_connect_test_client_id: 'acct_1EDHiJB7jgqs0lta',
+  stripe_test_secret_key: 'sk_test_nRai7UCweiVJYd7nIaHKgQ2X',
+  stripe_test_api_key: 'pk_test_sj4ltsm5aNdwM9jd0uNSsEmY'
 }
 
 export default config
